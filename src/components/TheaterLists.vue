ff54fc5a48dabfeacc117969dc9a088fd84b11f1
anusornleo <60070110@kmitl.ac.th>
2019-11-28 11:20:23 +0700

    edit

--- src/components/TheaterLists.vue
+++ src/components/TheaterLists.vue
@@ -33,7 +33,7 @@
             <input
               class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white"
               id="inline-full-name"
-              type="text"
+              type="number"
             />
           </div>
         </div>
